2f390868b111536b953f2a49e821351fcf52ac9b
vangeytedvg <vangeytedvg@gmail.com>
2020-05-01 13:16:47 +0200

    user management reset and mail

--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
+
 from . import views
 
 urlpatterns = [
@@ -6,6 +8,24 @@ urlpatterns = [
     path('register/', views.registerPage, name="register"),
     path('login/', views.loginPage, name="login"),
     path('logout/', views.logoutUser, name='logout'),
+
+    path('reset_password',
+         auth_views.PasswordResetView.as_view(
+             template_name="main/password_reset.html"),
+         name="reset_password"),
+    path('reset_password_sent',
+         auth_views.PasswordResetDoneView.as_view(
+             template_name="main/password_reset_sent.html"),
+         name="password_reset_done"),
+    path('reset/<uidb64>/<token>',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name="main/password_reset_form.html"),
+         name="password_reset_confirm"),
+    path('reset_password_complete',
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name="main/password_reset_done.html"),
+         name="password_reset_complete"),
+
 
     path('dispatch', views.dispatchTodo, name='dispatch'),
 
